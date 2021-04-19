189f083359e019fa693626f81fed8aa9a24fd824
ChenSingYo <chensingyo@gmail.com>
2021-04-19 15:54:06 +0800

    init: UserLogin page

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import UserLogin from './../views/UserLogin.vue'
+import UserRegist from './../views/UserRegist.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // root
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/login'
+  },
+  // user login
+  {
+    path: '/login',
+    name: 'user-login',
+    component: UserLogin
+  },
+  // user regist
+  {
+    path: '/regist',
+    name: 'user-regist',
+    component: UserRegist
+  },
+  // not found
   {
     path: '*',
     name: 'not-found',

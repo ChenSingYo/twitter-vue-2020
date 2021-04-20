8a25804c1f00d70b1677cc762b1492c14ec0d6e0
ChenSingYo <chensingyo@gmail.com>
2021-04-20 15:18:10 +0800

    style: css refine
    style: css style refine

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -2,11 +2,11 @@
   <div class="container">
     <!-- logo bar-->
     <div class="logo-bar header">
-      <img src="../assets/icon/Icon.png" alt="logo" />
+      <img src="../assets/icon/Icon.png" alt="logo" width="40px"/>
     </div>
     <!-- title bar-->
     <div class="title-bar header">
-      <span>登入Alphitter</span>
+      <span>登入 Alphitter</span>
     </div>
     <form @submit.prevent.stop="handleSubmit">
       <!-- 帳號 -->
@@ -37,7 +37,7 @@
           required
         />
       </div>
-      <div class="form-label-group button-div">
+      <div class="form-label-group button-wrapper">
         <button
           class="btn btn-primary btn-block submit-button"
           :disabled="isProcessing"
@@ -47,11 +47,11 @@
         </button>
       </div>
       <div class="form-bottom">
-        <router-link class="text-secondary" to="/regist">
+        <router-link class="text-link" to="/regist">
           註冊 Alphitter
         </router-link>
-        <span class="text-muted"> · </span>
-        <router-link class="text-secondary" to="/adminlogin">
+        <span>·</span>
+        <router-link class="text-link" to="/admin-login">
           後台登入
         </router-link>
       </div>
@@ -74,59 +74,92 @@ export default {
   }
 }
 </script>
+
 <style lang="scss" scoped>
+
 .container {
   margin-top: 50px;
   width: 50%;
   justify-content: center;
 
   .header {
-  text-align: center;
-  margin-bottom: 20px;
+    text-align: center;
+    margin-bottom: 30px;
   }
   .title-bar {
-  font-weight: 700;
+    margin-bottom: 20px;
+    font-weight: bold;
+    span {
+      font-size: 23px;
+    }
   }
   .form-label-group {
+    border-radius: 4px;
     position: relative;
     height: 50px;
-    margin-bottom: 30px;
+    margin-bottom: 20px;
     background: #f5f8fa;
     display: flex;
     align-items: flex-end;
     justify-content: center;
 
     label {
+      color: #657786;
       position: absolute;
-      font-size: .8rem;
+      font-size: 15px;
       height: 25px;
       width: 100%;
-      padding: 5px 0 0 10px;
+      padding: 3px 0 0 8px;
       top: 0;
       cursor: text;
     }
     .form-control {
+      border: none;
+      border-bottom: 3px solid #657786;
+      border-bottom-right-radius: 2px;
+      border-bottom-left-radius: 2px;
       height: 25px;
       background: #f5f8fa;
       padding: 0px 10px;
       outline-width: 0;
+
+      .form-bottom {
+        text-align: end;
+      }
     }
-    .button-div {
-      height: auto;
-      background: none;
-      display: flex;
-      flex-flow: column;
-      align-items: center;
-    }
-    .submit-button {
-      margin-bottom: 20px;
-    }
-    .form-bottom {
-      text-align: end;
-    }
-    .form-control:focus {
+    .form-control:focus,
+    .form-control:hover {
       box-shadow: none;
       border-color: #ff6600;
+    }
+  }
+  .button-wrapper {
+    height: 50px;
+    padding-top: 10px;
+    margin-bottom: 20px;
+    background: none;
+    flex-flow: column;
+    align-items: center;
+
+    .submit-button {
+      font-weight: 500;
+      height: 50px;
+      border: none;
+      border-radius: 50px;
+      color:white;
+      background-color:#ff6600;
+    }
+  }
+  .form-bottom {
+    text-align: end;
+    .text-link {
+      color: #0099FF;
+      text-decoration: underline;
+      font-weight: 900;
+    }
+    span {
+      color: #0099FF;
+      font-weight: bolder;
     }
   }
 }

1aec85d3d68229208304f2d717357113b4c15a01
ChenSingYo <chensingyo@gmail.com>
2021-04-20 16:02:38 +0800

    style: add css style

--- src/views/UserRegist.vue
+++ src/views/UserRegist.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <!-- logo bar-->
     <div class="logo-bar header">
-      <img src="../assets/icon/Icon.png" alt="logo">
+      <img src="../assets/icon/Icon.png" alt="logo" />
     </div>
     <!-- title bar-->
     <div class="title-bar header">
       <span>建立你的帳號</span>
     </div>
-    <form @submit.prevent.stop="handleFormSubmit">
+    <form @submit.prevent.stop="handleSubmit">
       <!-- 帳號 -->
       <div class="form-label-group">
         <label for="account">帳號</label>
@@ -75,14 +75,110 @@
           autocomplete="new-password"
         />
       </div>
-      <div class="form-label-group button-div">
-        <button class="btn btn-primary btn-block submit-button" :disabled="isProcessing" type="submit">
+      <div class="form-label-group button-wrapper">
+        <button
+          class="btn btn-primary btn-block submit-button"
+          :disabled="isProcessing"
+          type="submit"
+        >
           註冊
         </button>
-        <router-link class="text-secondary" v-show="!isProcessing" to="/signin">
+        <router-link class="" v-show="!isProcessing" to="/login">
           取消
         </router-link>
       </div>
     </form>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+.container {
+  margin-top: 50px;
+  width: 50%;
+  justify-content: center;
+
+  .header {
+    text-align: center;
+    margin-bottom: 30px;
+    img {
+      width: 40px;
+    }
+  }
+  .title-bar {
+    margin-bottom: 20px;
+    font-weight: bold;
+    span {
+      font-size: 23px;
+    }
+  }
+  .form-label-group {
+    border-radius: 4px;
+    position: relative;
+    height: 50px;
+    margin-bottom: 20px;
+    background: #f5f8fa;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+
+    label {
+      color: #657786;
+      position: absolute;
+      font-size: .8rem;
+      font-size: 15px;
+      height: 25px;
+      width: 100%;
+      padding: 3px 0 0 8px;
+      top: 0;
+      cursor: text;
+    }
+    .form-control {
+      border: none;
+      border-bottom: 3px solid #657786;
+      border-bottom-right-radius: 2px;
+      border-bottom-left-radius: 2px;
+      height: 25px;
+      background: #f5f8fa;
+      padding: 0px 10px;
+      outline-width: 0;
+    }
+    .form-control:hover,
+    .form-control:focus {
+      box-shadow: none;
+      border-color: #ff6600;
+    }
+  }
+  .button-wrapper {
+    size: 18px;
+    position: relative;
+    background: none;
+    align-items: center;
+
+    .submit-button {
+      font-weight: 500;
+      height: 50px;
+      border-radius: 50px;
+      top: 10px;
+      position: absolute;
+      border: none;
+      color:white;
+      background-color:#ff6600;
+    }
+  }
+  .button-wrapper :last-child {
+    top: 75px;
+    position: absolute;
+    color: #0099FF;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+}
+
+/* mobile 尺寸 */
+@media screen and (max-width: 768px) {
+  .container {
+    width: 80%;
+  }
+}
+</style>

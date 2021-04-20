496c4b7c8ba4f4fac3456272acfa740139578ae1
ChenSingYo <chensingyo@gmail.com>
2021-04-20 18:04:43 +0800

    add: template

---
+++ src/views/AdminTweets.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="main">
+    <div class="column-left column">
+      <AdminNavbar />
+    </div>
+    <div class="column-main column">
+      <div class="card main-content">
+        <ul class="list-group list-group-flush">
+          <Header />
+          <p class="loading" v-if="isLoading">Loading...</p>
+          <TweetCard
+            v-else
+            v-for="tweet in tweets"
+            :key="tweet.id"
+            :initial-tweet="tweet"
+            @after-delete-tweet="handleAfterDeleteTweet"
+          />
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminSidebar from '../components/AdminSidebar'
+export default {
+  name: 'AdminTweets',
+  component: {
+    AdminSidebar
+  }
+}
+</script>

99e0ca828b9e1bc4e61f00e131711d49b321689b
ChenSingYo <chensingyo@gmail.com>
2021-04-22 13:53:09 +0800

    add style

--- src/components/AdminTweetList.vue
+++ src/components/AdminTweetList.vue
@@ -1,30 +1,68 @@
 <template>
-  <div class="list-group-item">
-    <div class="card">
-      <div class="card-avartar">
-        <!-- avatar -->
-          <img src="" alt="">
+  <div class="cell cell-container">
+    <img class="avatar" src="https://picsum.photos/45" />
+      <p class="loading" v-if="false">Loading...</p>
+      <div class="info-container">
+        <span class="name">Apple</span>
+        <span class="tag">@apple</span>
+        <span class="time">1 小時</span>
+        <p class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          mollitia aliquid voluptas corporis voluptatibus exercitationem!
+          Consequatur dolor repellat.
+        </p>
       </div>
-      <div class="card-content">
-        <!-- tweetCard header -->
-        <div class="card-content-header">
-          <span class="user-name">Username</span>
-          <span class="text-muted">@Useraccount</span>
-          <span class="text-muted">time</span>
-          <button
-          type="button"
-          class="close"
-          >
-            <span>&times;</span>
-          </button>
-        </div>
-        <!-- tweet description -->
-        <div class="card-content-body">
-          <a v-if="true">
-            <p>tweet.description</p>
-          </a>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AdminTweetList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.cell-container {
+  display: flex;
+  padding: 10px 0;
+  border-bottom: 1px solid var(--light-gary-clr);
+
+  .avatar {
+    margin: 3px 15px 10px 15px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  .name {
+    color: var(--black-clr);
+    font-weight: 700;
+    font-size: 0.937rem;
+  }
+
+  .tag,
+  .time {
+    margin-left: 5px;
+    color: var(--cement-gary-clr);
+    font-weight: medium;
+    font-size: 0.937rem;
+  }
+
+  .time {
+    &::before {
+      display: inline-block;
+      font-weight: bold;
+      content: '・';
+    }
+  }
+
+  .content {
+    margin-bottom: 10px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 22px;
+  }
+}
+</style>
--- src/views/AdminTweets.vue
+++ src/views/AdminTweets.vue
@@ -1,34 +1,88 @@
-
 <template>
-  <div class="main">
-    <div class="column-left column">
-      <AdminSidebar />
-    </div>
-    <div class="column-main column">
-      <div class="card main-content">
-        <ul class="list-group list-group-flush">
-          <p class="loading" v-if="isLoading">Loading...</p>
-          <AdminTweetList />
-        </ul>
+  <div class="wrapper">
+    <AdminSidebar class="admin-sidebar" />
+    <div class="center-column">
+      <div class="header">
+        <div class="header-title">推文清單</div>
+      </div>
+      <div class="tweet-list">
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
+        <AminTweetList/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import AdminSidebar from '../components/AdminSidebar'
-import AdminTweetList from '../components/AdminTweetList'
+import AdminSidebar from './../components/AdminSidebar'
+import AminTweetList from './../components/AdminTweetList'
 
 export default {
   name: 'AdminTweets',
   components: {
     AdminSidebar,
-    AdminTweetList
+    AminTweetList
   },
   data () {
     return {
-      isLoading: false
+      tweets: []
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.wrapper {
+  display: grid;
+  width: 100vw;
+  grid-template-columns: 26.2% 1fr;
+  grid-template-rows: 100vh;
+
+  .admin-sidebar {
+    position: fixed;
+    z-index: 1;
+    grid-column: 1 / 1;
+    grid-row: 1 / 2;
+    width: 26.2%;
+    height: 100vh;
+  }
+
+  .center-column {
+    grid-column: 2 / 3;
+    grid-row: 1 / 2 ;
+    border-left: 1px solid #e6ecf0;
+  }
+
+  .header {
+    position: fixed;
+    z-index: 1;
+    width: 100vw;
+    height: 55px;
+    background: #fff;
+    border-bottom: 1px solid #e6ecf0;
+
+    .header-title {
+      margin-left: 20px;
+      line-height: 55px;
+      font-weight: 700;
+      font-size: 18px;
+      color: #1c1c1c;
+    }
+  }
+
+  .tweet-list {
+    margin-top: 70px;
+  }
+}
+</style>
